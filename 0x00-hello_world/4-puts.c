2d0a83eff57b76543b61c2e56a38c8df331334da
Your Name <you@example.com>
2023-09-08 04:29:44 -0700

    prints to stdout

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main: prints out a string to the stout 
+ *main: prints out a string to the stout
  *return 0 if success
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
