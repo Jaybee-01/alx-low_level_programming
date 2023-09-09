20ff3d9aaf0ee9553f271ff55c9a969290af346d
Your Name <you@example.com>
2023-09-08 21:53:37 -0700

    Numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main: program that prints all single digit numbers of *base 10
+ *Description: All your code should be withing the main *function
+ *Return: 0
+ */
+
+int main(void)
+{
+int n;
+while (n < 10);
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
