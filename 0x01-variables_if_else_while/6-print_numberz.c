f1fd7151fa26461f731a6d003cfd6c3b0d219cc1
Your Name <you@example.com>
2023-09-09 07:47:47 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry Poin
+ *
+ *Return: Always ) (Success)
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
