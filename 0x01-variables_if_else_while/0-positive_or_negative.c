8c6456b1a2aaccd3e3e842d2d507e9d68307569d
Your Name <you@example.com>
2023-09-08 19:05:03 -0700

    Pos or Neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,15 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- *main - this the main block
- *Description: check and prints number
- *that is either positive or negative
- *Return: 0 always
+ *main: main block
+ *Description: gets and check number
+ *if its positive or not
+ *return 0 always
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -18,11 +19,11 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-	printf("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 	else
 	{
-	printf("%i is zero\n", n);
+		printf("%i is zero\n, n);
 	}
 	return (0);
 }
