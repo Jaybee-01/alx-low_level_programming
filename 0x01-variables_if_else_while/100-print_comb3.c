93271f52a58b252f53601fe164199bac19169387
Your Name <you@example.com>
2023-09-09 08:56:16 -0700

    rogram that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ *main - program that prints all possible different combinations of two digits
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n, n1;
+
+	for (n = '0'; n < '9'; n1++)
+	{
+		for (n1 = '0'; n1 <= '9'; n1++)
+		{
+			if (n == '0' && n1 == '1')
+				continue;
+			if (n == '1' && n1 == '2')
+				continue;
+			if (n == '2' && n1 == '3')
+				continue;
+			if (n == '3' && n1 == '4')
+				continue;
+			if (n == '5' && n1 == '6')
+				continue;
+			if (n == '6' && n1 == '7')
+				continue;
+			if (n == '7' && n1 == '8')
+				continue;
+			if (n == '8' && n1 == '9')
+				continue;
+			putchar(n);
+			putchar(n1);
+			if (n == '8' && n1 < '9')
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
