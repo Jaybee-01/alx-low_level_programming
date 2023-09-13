dc5544e789313f31cab780f751c3c1b204886f45
Your Name <you@example.com>
2023-09-13 14:35:25 -0700

    Print sing

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *print_sign - fucntion that prints the sign of a number
+ *@n: the input to check
+ *Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar (45);
+		return (-1);
+	}
+	else
+	{
+		putchar (48);
+		return (0);
+	}
+}
