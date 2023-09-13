8c7c9b1c8d45f11ad63e431393f442e8dabac0c1
Your Name <you@example.com>
2023-09-13 15:03:17 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *print_last_digit - function that prints last digit
+ *@n: the inpuit number to check
+ *Return: 0
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+		last = last * -1;
+	putchar(last + '0');
+	return (last);
+}
