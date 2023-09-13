36d0137e52b66a6913ae971ffb327a2431a51803
Your Name <you@example.com>
2023-09-13 13:38:36 -0700

    Print alphabet is small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry Point
+ *Description: function that prints the alphabet, in lowercase.
+ *Return: 0
+ */
+void print_alphabet(void);
+{
+char = 'c';
+
+while (c <= 'z')
+{
+printf("\n", c );
+c++;
+}
+return print_alphabet;
+}
