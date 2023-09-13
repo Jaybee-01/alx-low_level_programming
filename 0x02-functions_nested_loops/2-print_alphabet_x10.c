08aa0a5d081aebc36b4ca4d468f579217b2a557c
Your Name <you@example.com>
2023-09-13 14:19:43 -0700

    Print letters in 10 places

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *print_alphabet_x10 - prints alphabets lowercase in 10places
+ *
+ *Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
