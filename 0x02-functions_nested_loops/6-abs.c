ab1b0c4f8e72c64983fb9b8f5df9e565adebfccd
Your Name <you@example.com>
2023-09-13 14:55:39 -0700

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *_abs - functions that computes the value of an integer
+ *@x: the input nu,ber to check
+ *retunr: 0
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = * (-1);
+		return (x);
+	}
+	else
+		return (x);
+}
+
