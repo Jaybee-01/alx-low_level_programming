2f5a6f52c01c74d01938a79725b4d57a9aab3b39
Your Name <you@example.com>
2023-09-16 09:55:11 -0700

    My files

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+
+#endif
