078af47e05972cfcf49172c8f469831dbbb2defb
Your Name <you@example.com>
2023-09-18 16:04:30 -0700

    Prints str

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,15 @@
 /**
  *_puts - function that prints a string, followed by a new lin
  *@str: the string to print
+ *Return : void
  */
 
 void _puts(char *str)
 {
+	*str = 0;
+
 	while (*str != '\0')
-	{
 		_putchar(*str++);
-		_putchar('\n');
-	}
+	_putchar('\n');
+
 }
