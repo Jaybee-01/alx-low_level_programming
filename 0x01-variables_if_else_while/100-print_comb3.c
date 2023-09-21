412e94b4b0097193341d89c42b41c95de352f725
Your Name <you@example.com>
2023-09-21 11:54:20 -0700

    Print combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,50 +7,23 @@
 
 int main(void)
 {
-	int n, n1;
+	int n = '0';
+	int n1 = '0';
 
-	for (n = '0'; n < '9'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		for (n1 = '0'; n1 < '9'; n1++)
+		for (n1 = '0'; n1 <= '9'; n1++)
 		{
-			if (n == '0' && n1 == '1')
-				continue;
-			if (n == '1' && n1 == '2')
-				continue;
-			if (n == '2' && n1 == '3')
-				continue;
-			if (n == '3' && n1 == '4')
-				continue;
-			if (n == '5' && n1 == '6')
-				continue;
-			if (n == '6' && n1 == '7')
-				continue;
-			if (n == '7' && n1 == '8')
-				continue;
-			if (n == '8' && n1 == '9')
-				continue;
-			putchar(n);
-			putchar(n1);
-			if (n1 == '1' && n == '0')
-				break;
-			if (n1 == '2' && n == '1')
-				break;
-			if (n1 == '3' && n == '2')
-				break;
-			if (n1 == '4' && n == '3')
-				break;
-			if (n1 == '5' && n == '4')
-				break;
-			if (n1 == '6' && n == '5')
-				break;
-			if (n1 == '7' && n == '6')
-				break;
-			if (n1 == '8' && n == '7')
-				break;
-			if (n1 == '9' && n == '8')
-				break;
-			putchar(',');
-			putchar(' ');
+			if (!((n == n1) || (n1 > n)))
+			{
+				putchar(n);
+				putchar(n1);
+				if (!(n == '9' && n1 == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
