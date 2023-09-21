0da7cd3be0fbc83c4375e2d30ac6ca38a21c9739
Your Name <you@example.com>
2023-09-21 12:30:27 -0700

    Concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ *_strcat - joins two strings together
+ *@dest: string one
+ *@src: string two
+ *Return: concatenate strings
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int a, b;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
 
