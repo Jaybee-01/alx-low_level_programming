c7bf61506e215b245579f2d4c4898e2b890270eb
Your Name <you@example.com>
2023-10-08 05:05:59 -0700

    prints number of argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  *@argv: list of argument
  *Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d, %s\n", argc, *argv);
+	printf("%d\n", argc - 1);
 	return (0);
 }
