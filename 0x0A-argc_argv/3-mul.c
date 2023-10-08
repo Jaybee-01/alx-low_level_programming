17283582bd75041260936ab07b5b0a1d0cb2ec40
Your Name <you@example.com>
2023-10-08 07:51:59 -0700

    muiltiplies the argument it pass

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -54,12 +54,12 @@ int _atoi(char *s)
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	
+
 	if (argc > 3 || argc < 3)
 	{
 		printf("Error\n");
+		return (1);
 	}
-	
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
