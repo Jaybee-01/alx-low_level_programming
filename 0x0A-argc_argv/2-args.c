4ae64f0406d60adc6a9c56ece2bb593a5ff175af
Your Name <you@example.com>
2023-10-08 05:37:18 -0700

    program that prints all argument it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ *main - program prints the arguments it receives
+ *@argc: number of argument
+ *@argv: list of argument
+ *Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int count;
-	for(count = 0; count < argc; count++)
+
+	for (count = 0; count < argc; count++)
 	{
 		printf("%s\n", argv[count]);
 	}
