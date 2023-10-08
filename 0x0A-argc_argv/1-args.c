e18462462cd4229ea65e0f943face2a6b8cd3ebc
Your Name <you@example.com>
2023-10-08 04:41:37 -0700

    prints the number of arg pass to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - program
+ *@argc: number of argument
+ *@argv: list of argument
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d, %s\n", argc, *argv);
+	return (0);
+}
