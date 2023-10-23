6aef0e7d1ea27477f3cbe3d5625ee8b9f67cf1b1
Your Name <you@example.com>
2023-10-23 13:11:18 -0700

    task 2 done

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in the new node created
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newwjay;
+
+	newwjay = malloc(sizeof(listint_t));
+
+	if (!newwjay)
+		return (NULL);
+
+	newwjay->n = n;
+	newwjay->next = *head;
+	*head = newwjay;
+
+	return (*head);
+}
