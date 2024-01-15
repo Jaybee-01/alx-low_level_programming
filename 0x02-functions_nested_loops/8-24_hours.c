9c4dbe0ace0822f19bf8351a1dfc66dcc6d49b01
Your Name <you@example.com>
2024-01-14 22:00:30 -0800

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 23; hours++)
+	{
+		for (minutes = 0; minutes < 10; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
