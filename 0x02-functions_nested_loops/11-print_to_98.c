4005109e8ae8112826ed480dc2b49807367fe256
Your Name <you@example.com>
2024-01-18 19:05:08 -0800

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
+#include <stdio.h>#
 #include "main.h"
+
 /**
  * print_to_98 - function that print number
  * @n: integer variaible
  * Return: success
  */
-
 void print_to_98(int n)
 {
 	int j = 98;
-
+	
 	if (n <= 98)
 	{
 		while (n < j)
 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
-       	else 
+	else
 	{
 		while (n > j)
 			printf("%d, ", n--);
