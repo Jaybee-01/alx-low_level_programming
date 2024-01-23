504f680a440453e5ff9bb23f78e21c4924a5a7c4
Your Name <you@example.com>
2024-01-23 10:01:44 -0800

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
- *
+ * print_numbers - prints 0 - 9
  *
  */
-
 void print_numbers(void)
-{ 
+{
 	int j;
 
 	j = '0';
-
 	while (j <= '9')
 	{
 		_putchar(j);
 		j++;
 	}
 	_putchar('\n');
-	return;
 }
